a66a0984ca818d13fd5ab2eba793871a6ab90d56
Rulrulmo <hwan901014@gmail.com>
2025-07-02 17:35:30 +0900

    기본형과 참조형 #2

---
+++ src/ref/InitData.java
@@ -0,0 +1,7 @@
+package ref;
+
+public class InitData {
+    int value1; // 초기화 X
+    int value2 = 10; // 초기화
+
+}
--- src/ref/VarChange1.java
+++ src/ref/VarChange1.java
@@ -2,19 +2,23 @@ package ref;
 
 public class VarChange1 {
     public static void main(String[] args) {
+        // 초기값 설정
         int a = 10;
-        int b = a;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        //a 변경
+        int b = a;  // a의 값(10)을 b에 복사 - 값 복사가 일어남
+
+        System.out.println("a = " + a);  // a = 10
+        System.out.println("b = " + b);  // b = 10
+
+        // a 변경 - b에는 영향 없음
         a = 20;
         System.out.println("변경 a = 20");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        //b 변경
+        System.out.println("a = " + a);  // a = 20
+        System.out.println("b = " + b);  // b = 10 (변경되지 않음)
+
+        // b 변경 - a에는 영향 없음
         b = 30;
         System.out.println("변경 b = 30");
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("a = " + a);  // a = 20 (변경되지 않음)
+        System.out.println("b = " + b);  // b = 30
     }
 }
