c6104769f0a6f727d4d67cc25dd92cfaf528c9ba
Rulrulmo <hwan901014@gmail.com>
2025-07-11 10:32:11 +0900

    상속 #1

---
+++ src/extends1/overriding/ElectricCar.java
@@ -0,0 +1,12 @@
+package extends1.overriding;
+
+public class ElectricCar extends Car {
+    @Override
+    public void move() {
+        System.out.println("Car move fast");
+    }
+
+    public void charge() {
+        System.out.println("charge");
+    }
+}
---
+++ src/final1/MemberMain.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class MemberMain {
+    public static void main(String[] args) {
+        Member member = new Member("1", "khmin");
+        member.print();
+        member.changeData("2", "hwan");
+        member.print();
+
+    }
+}
