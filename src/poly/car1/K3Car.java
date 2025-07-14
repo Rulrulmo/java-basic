3bd9e72c763913c3b211872d1319782ba24bf758
Rulrulmo <hwan901014@gmail.com>
2025-07-14 16:45:48 +0900

    다형성과 설계

---
+++ src/poly/car1/K3Car.java
@@ -0,0 +1,20 @@
+package poly.car1;
+
+public class K3Car implements Car {
+
+
+    @Override
+    public void startEngine() {
+        System.out.println("K3Car starting engine ...");
+    }
+
+    @Override
+    public void offEngine() {
+        System.out.println("K3Car off engine ...");
+    }
+
+    @Override
+    public void pressAccelerator() {
+        System.out.println("K3Car pressing accelerator ...");
+    }
+}
